1ffba92d77b3ea2e4156210892e03ecab40cc117
Elliot Coyle <elliotcoyle@ekmsystems.co.uk>
2018-01-15 11:59:45 +0000

    Fixing missing files

---
+++ KnowYourLimits/Identity/IClientIdentityProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace KnowYourLimits.Identity
+{
+    /// <summary>
+    /// Fetches a <see cref="IClientIdentity"/> for the current request.
+    /// </summary>
+    public interface IClientIdentityProvider<out TIdentityType> where TIdentityType : IClientIdentity
+    {
+        /// <summary>
+        /// Fetches a <see cref="IClientIdentity"/> for the current request.
+        /// </summary>
+        /// <returns>The current requests <see cref="IClientIdentity"/></returns>
+        TIdentityType GetIdentityForCurrentRequest();
+    }
+}
---
+++ KnowYourLimits/Strategies/LeakyBucket/LeakyBucketClientIdentity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using KnowYourLimits.Identity;
+
+namespace KnowYourLimits.Strategies.LeakyBucket
+{
+    public class LeakyBucketClientIdentity : IClientIdentity
+    {
+        public bool Equals(IClientIdentity other) => other.UniqueIdentifier == UniqueIdentifier;
+        public string UniqueIdentifier { get; set; }
+        public List<LeakyBucketInterval> Intervals = new List<LeakyBucketInterval>();
+        public DateTime? LastLeak { get; set; }
+    }
+}
