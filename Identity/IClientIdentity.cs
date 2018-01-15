27362d0090a001b89ed8e064df81f8509f875936
Elliot Coyle <elliotcoyle@ekmsystems.co.uk>
2018-01-15 11:44:44 +0000

    Initial Commit

---
+++ Identity/IClientIdentity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KnowYourLimits.Identity
+{
+    /// <summary>
+    /// A unique representation of the current client for identification purposes.
+    /// </summary>
+    public interface IClientIdentity : IEquatable<IClientIdentity>
+    {
+        /// <summary>
+        /// A unique identifier for this current request client.
+        /// </summary>
+        string UniqueIdentifier { get; set;  }
+    }
+}
---
+++ Strategies/LeakyBucket/LeakyBucketConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using KnowYourLimits.Identity;
+
+namespace KnowYourLimits.Strategies.LeakyBucket
+{
+    public class LeakyBucketConfiguration
+    {
+        public int MaxRequests { get; set; }
+        public TimeSpan LeakRate { get; set; }
+        public IClientIdentityProvider<LeakyBucketClientIdentity> IdentityProvider { get; set; }
+    }
+}
